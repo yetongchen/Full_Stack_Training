1ee60ebba38d038b8ff62d03260a92ddcd63043a
Yetong <cissieslab@gmail.com>
2024-10-28 03:29:43 -0400

    remove mvc project

--- 04 Module Building Modern Web Applications using ASP .NET & Azure/MovieShopDbApp/MovieShopDbApp/MovieApp.WebMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MovieApp.WebMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
